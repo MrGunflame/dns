6dbd87f861ca43308d1f3b525dbb8a0b4306bf14
MrGunflame <mrgunflame@protonmail.com>
2025-09-01 20:57:17 +0200

    Handle RD flag set to 0

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,1 +1,53 @@
+use crate::proto::{OpCode, Packet, Qr, ResourceRecord, ResponseCode};
+use crate::state::State;
+
 pub mod udp;
+
+pub async fn handle_query(state: &State, packet: Packet) -> Option<Packet> {
+    let mut answers = Vec::new();
+    let mut response_code = ResponseCode::Ok;
+
+    if packet.recursion_desired {
+        for question in &packet.questions {
+            match state.resolve(question).await {
+                Ok(resp) => {
+                    for answer in resp {
+                        answers.push(ResourceRecord {
+                            r#type: answer.r#type,
+                            class: answer.class,
+                            ttl: answer.ttl().as_secs() as u32,
+                            rdata: answer.data,
+                            name: answer.name,
+                        });
+                    }
+                }
+                Err(err) => {
+                    tracing::error!("failed to resolve query: {:?}", err);
+
+                    // NOTE: The DNS standard is not clear how to handle
+                    // multiple questions in a single packet.
+                    // We attempt to handle all questions, but if any question
+                    // fails to resolve we return no answers.
+                    answers.clear();
+                    response_code = ResponseCode::ServerFailure;
+                    break;
+                }
+            };
+        }
+    }
+
+    Some(Packet {
+        transaction_id: packet.transaction_id,
+        qr: Qr::Response,
+        opcode: OpCode::Query,
+        authoritative_answer: false,
+        recursion_desired: packet.recursion_desired,
+        recursion_available: true,
+        truncated: false,
+        response_code,
+        questions: packet.questions,
+        answers,
+        additional: Vec::new(),
+        authority: Vec::new(),
+    })
+}
--- src/frontend/udp.rs
+++ src/frontend/udp.rs
@@ -5,7 +5,8 @@ use futures::stream::{FuturesOrdered, StreamExt};
 use futures::{select_biased, FutureExt};
 use tokio::net::UdpSocket;
 
-use crate::proto::{OpCode, Packet, Qr, ResourceRecord, ResponseCode};
+use crate::frontend::handle_query;
+use crate::proto::Packet;
 use crate::state::State;
 
 #[derive(Debug)]
@@ -66,53 +67,12 @@ impl UdpServer {
 }
 
 async fn handle_request(packet: Packet, addr: SocketAddr, socket: &UdpSocket, state: &State) {
-    let mut answers = Vec::new();
-    let mut response_code = ResponseCode::Ok;
-
-    for question in &packet.questions {
-        match state.resolve(question).await {
-            Ok(resp) => {
-                for answer in resp {
-                    answers.push(ResourceRecord {
-                        r#type: answer.r#type,
-                        class: answer.class,
-                        ttl: answer.ttl().as_secs() as u32,
-                        rdata: answer.data,
-                        name: answer.name,
-                    });
-                }
-            }
-            Err(err) => {
-                tracing::error!("failed to resolve query: {:?}", err);
-
-                // NOTE: The DNS standard is not clear how to handle
-                // multiple questions in a single packet.
-                // We attempt to handle all questions, but if any question
-                // fails to resolve we return no answers.
-                answers.clear();
-                response_code = ResponseCode::ServerFailure;
-                break;
-            }
-        };
-    }
-
-    let response = Packet {
-        transaction_id: packet.transaction_id,
-        qr: Qr::Response,
-        opcode: OpCode::Query,
-        authoritative_answer: false,
-        recursion_desired: packet.recursion_desired,
-        recursion_available: true,
-        truncated: false,
-        response_code,
-        questions: packet.questions,
-        answers,
-        additional: Vec::new(),
-        authority: Vec::new(),
+    let Some(resp) = handle_query(state, packet).await else {
+        return;
     };
 
     let mut buf = Vec::new();
-    response.encode(&mut buf);
+    resp.encode(&mut buf);
 
     if let Err(err) = socket.send_to(&buf, addr).await {
         tracing::debug!("failed to respond to {}: {}", addr, err);
